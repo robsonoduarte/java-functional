f1e756b3c3db0d27ffd9249ca10f35e8169b9875
Robson Duarte <robson.o.d@gmail.com>
2017-05-25 20:10:15 -0300

    add functional style example.

---
+++ hello-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DiscountFunctional.java
@@ -0,0 +1,22 @@
+package br.com.mystudies.java.functional;
+
+import static java.math.BigDecimal.ZERO;
+import static java.math.BigDecimal.valueOf;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class DiscountFunctional {
+
+
+	public BigDecimal apply(List<BigDecimal> list) {
+		return list.stream()
+				.filter(p -> p.compareTo(valueOf(20)) > 0)
+				.map(p -> p.multiply(valueOf(0.9)))
+				.reduce(ZERO, BigDecimal::add);
+
+	}
+
+
+
+}
---
+++ hello-lambda-expressions/src/test/java/br/com/mystudies/java/functional/DiscountFunctionalTest.java
@@ -0,0 +1,40 @@
+package br.com.mystudies.java.functional;
+
+import static java.math.BigDecimal.valueOf;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DiscountFunctionalTest {
+
+	private DiscountFunctional discountFunctional;
+
+
+	@Before
+	public void setUp() throws Exception {
+		discountFunctional = new DiscountFunctional();
+	}
+
+
+
+
+	@Test
+	public void test() {
+	 final List<BigDecimal> list = asList(
+			 new BigDecimal("10"),new BigDecimal("30"),new BigDecimal("17"),
+			 new BigDecimal("20"),new BigDecimal("15"),new BigDecimal("18"),
+			 new BigDecimal("45"),new BigDecimal("12")
+			 );
+
+	  assertEquals(valueOf(67.5), discountFunctional.apply(list));
+	}
+
+
+
+
+}
