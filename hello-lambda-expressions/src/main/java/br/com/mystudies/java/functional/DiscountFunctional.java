dc02b36f74d85af6b81f168f1653998a0d45d15d
Robson Duarte <robson.o.d@gmail.com>
2017-05-25 20:19:31 -0300

    Update DiscountFunctional.java
    format code.

--- hello-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DiscountFunctional.java
+++ hello-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DiscountFunctional.java
@@ -8,15 +8,12 @@ import java.util.List;
 
 public class DiscountFunctional {
 
-
 	public BigDecimal apply(List<BigDecimal> list) {
 		return list.stream()
-				.filter(p -> p.compareTo(valueOf(20)) > 0)
-				.map(p -> p.multiply(valueOf(0.9)))
-				.reduce(ZERO, BigDecimal::add);
+			.filter(p -> p.compareTo(valueOf(20)) > 0)
+			.map(p -> p.multiply(valueOf(0.9)))
+			.reduce(ZERO, BigDecimal::add);
 
 	}
-
-
 
 }
