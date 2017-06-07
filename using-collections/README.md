7425c6e9dd4a5e075671af99c8e29291c179d7fd
Robson Duarte <robson.o.d@gmail.com>
2017-06-07 19:28:53 -0300

    initial import

---
+++ using-collections/README.md
@@ -0,0 +1,3 @@
+Hello, Lambada Expressions
+--------------------------
+In this simple project we can see the differences between [Imperative Style Programming](https://github.com/robsonoduarte/java-functional/blob/master/hello-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DiscountImperative.java) and [Functional Style Programming](https://github.com/robsonoduarte/java-functional/blob/master/hello-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DiscountFunctional.java)
---
+++ using-collections/src/main/java/br/com/mystudies/java/functional/IteratingThroughAList.java
@@ -0,0 +1,55 @@
+package br.com.mystudies.java.functional;
+
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class IteratingThroughAList {
+
+
+	public static void main(String[] args) {
+
+		final List<String> friends =
+				asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+
+		// the habitual, but not so desirable, way to iterate
+		for (int i = 0; i < friends.size(); i++) {
+			System.out.println(friends.get(i));
+		}
+
+		// foreach ( since java 5 )
+		for (String name : friends) {
+			System.out.println(name);
+		}
+
+		// foreach() of Interable interface ( since java 8 ) with the anonymous inner class
+
+		friends.forEach(new Consumer<String>() {
+			@Override
+			public void accept(final String name) {
+				System.out.println(name);
+			}
+		});
+
+
+		// foreach() with lambda expression
+		friends.forEach((final String name) -> System.out.println(name));
+
+		// compiler can infer the type
+		friends.forEach((name) -> System.out.println(name));
+
+		// compiler treats single-parameter lambda expression as special
+		friends.forEach(name -> System.out.println(name));
+
+
+		// with method reference
+		friends.forEach(System.out::println);
+
+
+	}
+
+
+
+
+}
