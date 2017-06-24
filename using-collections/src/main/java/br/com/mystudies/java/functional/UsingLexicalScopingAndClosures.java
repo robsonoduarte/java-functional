970bc6e9ce1f18c4a9998ed68d4d7dd0a59d5cf5
Robson Duarte <robson.o.d@gmail.com>
2017-06-23 21:14:56 -0300

    continue the example.

--- using-collections/src/main/java/br/com/mystudies/java/functional/UsingLexicalScopingAndClosures.java
+++ using-collections/src/main/java/br/com/mystudies/java/functional/UsingLexicalScopingAndClosures.java
@@ -1,11 +1,40 @@
 package br.com.mystudies.java.functional;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+import java.util.function.Predicate;
+
 public class UsingLexicalScopingAndClosures {
 
 
 	public static void main(String[] args) {
 
+		final List<String> friends =
+				asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+
+
+		// Duplication in Lambada Expressions
+
+		final Predicate<String> startsWithB = name -> name.startsWith("B");
+		final Predicate<String> startsWithN = name -> name.startsWith("N");
+
+
+		System.out.println(friends.stream().filter(startsWithB).count());
+		System.out.println(friends.stream().filter(startsWithN).count());
+
+
+		// Removing Duplication with Lexical Scoping
+
+		System.out.println(friends.stream().filter(startsWith("B")).count());
+		System.out.println(friends.stream().filter(startsWith("N")).count());
+
+
 	}
 
+
+	public static Predicate<String> startsWith(final String letter){
+		return name -> name.startsWith(letter);
+	}
 
 }
