b24e1889410bc85587d0ba7da0c06000bb944ea1
Robson Duarte <robson.o.d@gmail.com>
2017-07-03 17:03:05 -0300

    starting example.

--- using-collections/src/main/java/br/com/mystudies/java/functional/ReducingACollectionToASingleValue.java
+++ using-collections/src/main/java/br/com/mystudies/java/functional/ReducingACollectionToASingleValue.java
@@ -1,8 +1,14 @@
 package br.com.mystudies.java.functional;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
 public class ReducingACollectionToASingleValue {
 
 	public static void main(String[] args) {
 
+		final List<String> friends =
+				asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
 	}
 }
