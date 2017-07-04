a2413263b16fae79c5ab79512759e20bcd9b61e8
Robson Duarte <robson.o.d@gmail.com>
2017-07-04 20:37:15 -0300

    finished the example.

--- using-collections/src/main/java/br/com/mystudies/java/functional/ReducingACollectionToASingleValue.java
+++ using-collections/src/main/java/br/com/mystudies/java/functional/ReducingACollectionToASingleValue.java
@@ -1,8 +1,11 @@
 package br.com.mystudies.java.functional;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
 
 public class ReducingACollectionToASingleValue {
 
@@ -10,6 +13,38 @@ public class ReducingACollectionToASingleValue {
 
 		final List<String> friends =
 				asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+
+
+		// example of map/reduce using the mapToInt and sum
+		System.out.println("Total number of characters in all names: " +
+		 friends.stream()
+			.mapToInt(String::length)
+			.sum());
+
+
+
+
+		// common lambda for next examples
+		BinaryOperator<String> isLongerThan = (n1,n2) -> n1.length() >= n2.length() ? n1 : n2;
+
+
+		// example of reduce using the reduce method
+		Optional<String> aLongName =
+				friends.stream()
+					.reduce(isLongerThan);
+
+		aLongName.ifPresent(name -> System.out.println(format("A longest name: %s", name)));
+
+
+		// example of variation of reduce with default value
+
+		String steveOrLonger =
+				friends.stream()
+					.reduce("Steve", isLongerThan);
+
+		System.out.println("Steve or Longer -> " + steveOrLonger);
+
 	}
+
 
 }
