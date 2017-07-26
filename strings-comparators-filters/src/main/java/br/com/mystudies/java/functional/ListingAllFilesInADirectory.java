8dccf5cfb22063f8ab6dc64e58a2b1ec73b57d4a
Robson Duarte <robson.o.d@gmail.com>
2017-07-26 19:59:19 -0300

    add example how list files using the stream ( CloseableStream )

---
+++ strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingAllFilesInADirectory.java
@@ -0,0 +1,23 @@
+package br.com.mystudies.java.functional;
+
+import static java.nio.file.Paths.get;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ListingAllFilesInADirectory {
+
+	public static void main(String[] args) throws IOException {
+
+		// list all file in current directory
+		Files.list(get("."))
+			.forEach(System.out::println);
+
+
+		// using one filter
+		Files.list(get("."))
+			.filter(Files::isDirectory)
+			.forEach(System.out::println);
+
+	}
+}
