34ec74dcfaea5473ab15490ad8a21a2851c89494
Robson Duarte <robson.o.d@gmail.com>
2017-07-28 17:05:46 -0300

    add example how use the newDirectoryStream method

--- strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingSelectFilesInADirectory.java
+++ strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingSelectFilesInADirectory.java
@@ -1,16 +1,21 @@
 package br.com.mystudies.java.functional;
 
+import static java.nio.file.Paths.get;
 import static java.util.Arrays.asList;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ListingSelectFilesInADirectory {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 
-		// use File.list with Filter in anonymous class sintaxe
+		// use File.list with Filter in anonymous class syntax
 		final String[] files1 =
 			new File("./src/main/resources").list(new FilenameFilter() {
 				@Override
@@ -26,6 +31,34 @@ public class ListingSelectFilesInADirectory {
 				new File("./src/main/resources").list((dir,name) -> name.endsWith(".txt"));
 
 		asList(files2).forEach(System.out::println);
+
+	//--------------------------------------------------------------------------------------------------
+
+
+	 // using the Files.newDirectoryStream with anonymous class syntax
+		Files.newDirectoryStream(get("./src/main/resources"),
+			new DirectoryStream.Filter<Path>(){
+				@Override
+				public boolean accept(Path path) throws IOException {
+					return path.toString().endsWith(".txt");
+				}
+		}).forEach(System.out::println);
+
+
+	  // change to the lambda
+		Files.newDirectoryStream(
+				get("./src/main/resources"),p -> p.toString().endsWith(".txt")
+			).forEach(System.out::println);
+
+
+
+	//--------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 
 
