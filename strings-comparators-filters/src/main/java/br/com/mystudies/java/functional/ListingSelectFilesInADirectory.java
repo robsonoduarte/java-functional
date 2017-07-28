b570f74c394bd35fc215de643bfd1c31a3417cc5
Robson Duarte <robson.o.d@gmail.com>
2017-07-28 16:39:49 -0300

    add example how to list and filter files using the method list.

--- strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingSelectFilesInADirectory.java
+++ strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingSelectFilesInADirectory.java
@@ -1,5 +1,34 @@
 package br.com.mystudies.java.functional;
 
+import static java.util.Arrays.asList;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
 public class ListingSelectFilesInADirectory {
+
+	public static void main(String[] args) {
+
+
+		// use File.list with Filter in anonymous class sintaxe
+
+		String[] files =
+			new File("./src/main/resources").list(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith(".txt");
+				}
+			});
+
+		asList(files).forEach(System.out::println);
+
+
+
+
+
+
+
+	}
+
 
 }
