6d01842490ee70fa0b7b17639637fdb8e6be0194
Robson Duarte <robson.o.d@gmail.com>
2017-08-01 20:20:47 -0300

    add example how use the watch file with features of Java 7 and Java 8

---
+++ strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/WatchingAFileChange.java
@@ -0,0 +1,42 @@
+package br.com.mystudies.java.functional;
+
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.concurrent.TimeUnit;
+
+public class WatchingAFileChange {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+
+		final Path path = get("./src/main/resources"); // change some file in resources dir. to see work of Watch Service.
+
+		final WatchService watchService =
+				path.getFileSystem().newWatchService();
+
+
+		path.register(watchService, ENTRY_MODIFY);
+
+
+		System.out.println("Report any file changed within next 1 minute...");
+
+
+		WatchKey watchKey =
+				watchService.poll(1, TimeUnit.MINUTES);
+
+		if(watchKey != null){
+			watchKey.pollEvents()
+				.stream()
+				.forEach(event -> System.out.println(event.context()));
+		}
+
+	}
+
+
+
+}
