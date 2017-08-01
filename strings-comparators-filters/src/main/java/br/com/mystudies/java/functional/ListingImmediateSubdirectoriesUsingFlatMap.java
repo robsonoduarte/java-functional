9767071d7988a23cbd4d015a06c867995e4230ed
Robson Duarte <robson.o.d@gmail.com>
2017-08-01 19:45:16 -0300

    add example in functional sytle.

--- strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingImmediateSubdirectoriesUsingFlatMap.java
+++ strings-comparators-filters/src/main/java/br/com/mystudies/java/functional/ListingImmediateSubdirectoriesUsingFlatMap.java
@@ -1,6 +1,8 @@
 package br.com.mystudies.java.functional;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.of;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,7 +12,8 @@ public class ListingImmediateSubdirectoriesUsingFlatMap {
 
 
 	public static void main(String[] args) {
-		listTheHardway();
+		listInImperativeSytle();
+		listInFunctionalStyle();
 	}
 
 
@@ -20,7 +23,7 @@ public class ListingImmediateSubdirectoriesUsingFlatMap {
 
 
     // list files in the imperative style
-	public static void listTheHardway(){
+	public static void listInImperativeSytle(){
 		List<File> files = new ArrayList<>();
 
 		File[] filesInCurrentDir = new File(".").listFiles();
@@ -38,6 +41,27 @@ public class ListingImmediateSubdirectoriesUsingFlatMap {
 			System.out.println(file);
 		}
 	}
+
+
+
+
+	// list files in functional style using the flatmap method.
+	public static void listInFunctionalStyle(){
+		of(new File(".").listFiles())
+			.flatMap(file -> file.listFiles() == null ? of(file) : of(file.listFiles()))
+			.collect(toList())
+			.forEach(System.out::println);
+	}
+
+
+
+
+
+
+
+
+
+
 
 
 
