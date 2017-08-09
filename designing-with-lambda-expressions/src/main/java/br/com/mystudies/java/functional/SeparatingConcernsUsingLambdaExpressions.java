a87be83bf34259746e485684c6b903a4a1d02ed8
Robson Duarte <robson.o.d@gmail.com>
2017-08-09 20:03:07 -0300

    finished the example.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/SeparatingConcernsUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/SeparatingConcernsUsingLambdaExpressions.java
@@ -5,6 +5,7 @@ import static br.com.mystudies.java.functional.Asset.AssetType.STOCK;
 import static java.util.Arrays.asList;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 
 
@@ -19,9 +20,18 @@ public class SeparatingConcernsUsingLambdaExpressions {
 				new Asset(STOCK, 4000)
 		);
 
+		//Getting Entangled with the concerns ( duplicate code see the statics methods )
 		System.out.println("Total of all assets: " + totalAssetValues(assets));
 		System.out.println("Total of all bonds: " + totalBondValues(assets));
 		System.out.println("Total of all stock: " + totalStockValues(assets));
+
+
+
+		//Refactoring to Separate a Key Concern
+		System.out.println("Total of all assets: " + totalAssetValues(assets, asset -> true));
+		System.out.println("Total of all bonds: " + totalAssetValues(assets, asset -> BOND.equals(asset.type())));
+		System.out.println("Total of all stocks: " + totalAssetValues(assets, asset -> STOCK.equals(asset.type())));
+
 	}
 
 
@@ -29,8 +39,7 @@ public class SeparatingConcernsUsingLambdaExpressions {
 
 
 
-
-
+	// METHODS THE SAME CONCERNS
 	public static int totalAssetValues(final List<Asset> assets){
 		return assets.stream()
 					.mapToInt(Asset::value)
@@ -51,6 +60,18 @@ public class SeparatingConcernsUsingLambdaExpressions {
 				.filter(asset -> STOCK.equals(asset.type()))
 				.mapToInt(Asset::value)
 				.sum();
+	}
+
+
+
+
+
+	// Refactoring the methods above in one only method that receives the lambda expression
+	public static int totalAssetValues(final List<Asset> assets, final Predicate<Asset> assetSelector){
+		return assets.stream()
+					.filter(assetSelector)
+					.mapToInt(Asset::value)
+					.sum();
 	}
 
 
