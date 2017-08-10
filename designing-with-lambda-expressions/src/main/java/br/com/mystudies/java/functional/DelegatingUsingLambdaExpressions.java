fd170ed1c694ed4fb841658d4d4fea5085409648
Robson Duarte <robson.o.d@gmail.com>
2017-08-10 20:51:31 -0300

    continue the example...

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DelegatingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DelegatingUsingLambdaExpressions.java
@@ -1,5 +1,41 @@
 package br.com.mystudies.java.functional;
 
+import static java.math.BigDecimal.valueOf;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
 public class DelegatingUsingLambdaExpressions {
+
+
+	public static void main(String[] args) {
+
+	}
+
+
+
+
+	public class CalculateNAV{
+
+		private Function<String, BigDecimal> priceFinder;
+
+		public CalculateNAV(Function<String, BigDecimal> priceFinder) {
+			this.priceFinder = priceFinder;
+		}
+
+
+		public BigDecimal computeStockWorth(final String ticker, final int shares){
+			return priceFinder.apply(ticker).multiply(valueOf(shares));
+		}
+
+	}
+
+
+
+
+
+
+
+
 
 }
