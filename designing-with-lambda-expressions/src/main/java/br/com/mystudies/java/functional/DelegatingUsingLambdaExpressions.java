b66551e2af2dfe2ecfc08927908ac5d66ff7492f
Robson Duarte <robson.o.d@gmail.com>
2017-08-22 20:34:09 -0300

    add the url of google finance.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DelegatingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DelegatingUsingLambdaExpressions.java
@@ -1,7 +1,12 @@
 package br.com.mystudies.java.functional;
 
 import static java.math.BigDecimal.valueOf;
+import static java.net.URLEncoder.encode;
+import static java.time.LocalDate.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Locale.US;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.util.function.Function;
 
@@ -36,10 +41,30 @@ public class DelegatingUsingLambdaExpressions {
 
 	public static class GoogleFinance{
 		public static BigDecimal getPrice(final String ticker){
-			return valueOf(10.0);
+
+
+			try {
+				final String x =
+				"http://www.google.com/finance/historical?q=" + ticker + "&startdate=" + yesterday() + "&output=csv";
+
+				System.out.println(x);
+
+				/*final URL url = new URL("http://www.google.com/finance/historical?q=${symbol}&startdate=${yesterday}&output=csv");*/
+
+				return valueOf(10.0);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 
-		//"http://www.google.com/finance/historical?q=${symbol}&startdate=${yesterday}&output=csv"
+
+
+
+		private static String yesterday() throws UnsupportedEncodingException {
+			return encode(now().minusDays(1).format(ofPattern("MMM d, yyyy",US)),"UTF-8");
+		}
+
+
 	}
 
 }
