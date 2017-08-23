d3d1ff4fc52b75671c5e998c53730aac830cbb81
Robson Duarte <robson.o.d@gmail.com>
2017-08-23 20:37:10 -0300

    upercase in static field.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
@@ -12,13 +12,13 @@ import java.time.format.DateTimeFormatter;
 public class DecoratingUsingLambdaExpressions {
 
 
-	private static final DateTimeFormatter pattern = ofPattern("yyyy-MM-dd HH:mm:ss");
+	private static final DateTimeFormatter PATTERN = ofPattern("yyyy-MM-dd HH:mm:ss");
 
 
 	public static void main(String[] args) {
 
-		LocalDateTime d1 = parse("2017-08-17 20:58:00", pattern);
-		LocalDateTime d2 = parse("2017-08-17 20:59:00", pattern);
+		LocalDateTime d1 = parse("2017-08-17 20:58:00", PATTERN);
+		LocalDateTime d2 = parse("2017-08-17 20:59:00", PATTERN);
 
 		System.out.println(between(d1, d2).getSeconds());
 		// or
