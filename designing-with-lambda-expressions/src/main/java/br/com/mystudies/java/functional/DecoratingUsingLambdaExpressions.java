59f4a7a5fdd9facb54fb113409ddb81717eadac7
Robson Duarte <robson.o.d@gmail.com>
2017-08-23 20:19:38 -0300

    add temporary example how get the difference between to dates.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
@@ -1,5 +1,27 @@
 package br.com.mystudies.java.functional;
 
+
+import static java.time.LocalDateTime.parse;
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DecoratingUsingLambdaExpressions {
+
+
+	private static final DateTimeFormatter pattern = ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
+	public static void main(String[] args) {
+
+		LocalDateTime d1 = parse("2017-08-17 20:58:00", pattern);
+		LocalDateTime d2 = parse("2017-08-17 20:59:00", pattern);
+
+		System.out.println(Duration.between(d1, d2).getSeconds());
+	}
+
+
 
 }
