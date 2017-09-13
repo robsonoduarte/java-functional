c8914b460c1e7697e57d04d659405dd9bfe64fa6
Robson Duarte <robson.o.d@gmail.com>
2017-09-13 19:07:31 -0300

    add class Camera

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
@@ -1,14 +1,24 @@
 package br.com.mystudies.java.functional;
 
+import java.awt.Color;
+import java.util.function.Function;
+
 public class DecoratingUsingLambdaExpressions {
-
-
 
 
 
 	public static void main(String[] args) {
 	}
 
+
+	public static class Camera{
+		private Function<Color, Color> filter;
+
+		public Color capture(final Color inputColor){
+			final Color processedColor = filter.apply(inputColor);
+			return processedColor;
+		}
+	}
 
 
 }
