11e7a3460f69613eb86fdc873d6cc42fefa0b1c2
Robson Duarte <robson.o.d@gmail.com>
2017-09-13 19:21:21 -0300

    add setFilters.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
@@ -2,6 +2,7 @@ package br.com.mystudies.java.functional;
 
 import java.awt.Color;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class DecoratingUsingLambdaExpressions {
 
@@ -11,14 +12,35 @@ public class DecoratingUsingLambdaExpressions {
 	}
 
 
+
+
+
 	public static class Camera{
 		private Function<Color, Color> filter;
+
+
+		public Camera(){
+			setFilters();
+		}
 
 		public Color capture(final Color inputColor){
 			final Color processedColor = filter.apply(inputColor);
 			return processedColor;
 		}
+
+
+		public void setFilters(final Function<Color,Color>... filters){
+			filter =
+					Stream.of(filters)
+					.reduce((filter,next) -> filter.compose(next))
+					.orElse(color -> color);
+		}
+
 	}
+
+
+
+
 
 
 }
