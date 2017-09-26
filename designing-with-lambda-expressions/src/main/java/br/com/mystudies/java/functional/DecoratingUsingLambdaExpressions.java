b255ef556c04844df6ddf5563e5d9aca49823c3e
Robson Duarte <robson.o.d@gmail.com>
2017-09-26 16:34:41 -0300

    format code.

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/DecoratingUsingLambdaExpressions.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 public class DecoratingUsingLambdaExpressions {
 
 	public static void main(String[] args) {
+		
 		final Camera camera  = new Camera();
 
 		final Consumer<String> printCaptured = (filterInfo) ->
