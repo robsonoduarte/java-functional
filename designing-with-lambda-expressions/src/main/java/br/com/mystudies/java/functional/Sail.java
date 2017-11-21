5dc98dd896a69545bdafa2de2378e62d294fc72c
Robson Duarte <robson.o.d@gmail.com>
2017-11-21 18:05:15 -0200

    add class Vehicle

---
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/Sail.java
@@ -0,0 +1,7 @@
+package br.com.mystudies.java.functional;
+
+public interface Sail {
+
+	default void cruise(){System.out.println("Sail::cruise");}
+	default void turn(){System.out.println("Sail::turn");}
+}
