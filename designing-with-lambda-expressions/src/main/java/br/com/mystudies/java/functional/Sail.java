f857bb6105164c0656c05961790a2b480d6eed98
Robson Duarte <robson.o.d@gmail.com>
2017-11-23 18:27:29 -0200

    format code

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/Sail.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/Sail.java
@@ -3,4 +3,5 @@ package br.com.mystudies.java.functional;
 public interface Sail {
 	default void cruise(){System.out.println("Sail::cruise");}
 	default void turn(){System.out.println("Sail::turn");}
+
 }
