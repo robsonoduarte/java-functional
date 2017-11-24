42c142cfaa9dd22a05bdf4e81036ccd5c496c60e
Robso Duarte <robson.o.d@gmail.com>
2017-11-24 17:28:47 -0200

    format code

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/Fly.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/Fly.java
@@ -6,5 +6,4 @@ public interface Fly {
 	default void land(){System.out.println("Fly::land");}
 	default void turn(){System.out.println("Fly::turn");}
 	default void cruise(){System.out.println("Fly::cruise");}
-
 }
