889b1d53b53faad92270d36967e2f25aa30e3178
Robson Duarte <robson.o.d@gmail.com>
2018-01-24 19:28:21 -0200

    finished example

--- designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/APeekIntoTheDefaultMethod.java
+++ designing-with-lambda-expressions/src/main/java/br/com/mystudies/java/functional/APeekIntoTheDefaultMethod.java
@@ -4,14 +4,66 @@ public class APeekIntoTheDefaultMethod {
 
 	public static void main(String[] args) {
 
+		SeaPlane seaPlane = new APeekIntoTheDefaultMethod().seaPlane();
+		seaPlane.takeOff();
+		seaPlane.turn();
+		seaPlane.cruise();
+		seaPlane.land();
 	}
 	
 	
 	
 	
+	public SeaPlane seaPlane() {
+		return new SeaPlane();
+	}
+	
+	
+	
+	
+	class SeaPlane extends Vehicle implements FastFly, Sail{
+		 
+		 
+		 
+		private int altitude;	
+		/* 
+		 * the compiler will force implement the cruise() method, 
+		 * because the default implementations in the interfaces FastFly, and Sail conflict
+		*/
+		public void cruise() {
+			System.out.print("SeaPlane::cruise currently cruise like: ");
+			
+			if(altitude > 0)
+				FastFly.super.cruise();
+			else
+				Sail.super.cruise();
+		}	
+	}
 
 	
+
 	
+	public interface FastFly extends Fly {
+		default void takeOff(){System.out.println("FastFly::takeOff");}
+	}
+	
+	
+	public interface Sail {
+		default void cruise(){System.out.println("Sail::cruise");}
+		default void turn(){System.out.println("Sail::turn");}
+	}
+	
+	public interface Fly {
+		default void takeOff(){System.out.println("Fly::takeOff");}
+		default void land(){System.out.println("Fly::land");}
+		default void turn(){System.out.println("Fly::turn");}
+		default void cruise(){System.out.println("Fly::cruise");}
+	}
+	
+	public class Vehicle {
+		public void turn(){System.out.println("Vehicle::turn");}
+	}
+
 }
 
 
