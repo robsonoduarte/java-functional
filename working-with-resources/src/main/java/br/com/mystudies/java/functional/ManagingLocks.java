23cd334ee5bd4991f0dbcc42b7d2ca5cedb906d6
Robson Duarte <robson.o.d@gmail.com>
2018-02-21 19:08:38 -0300

    stop the example

--- working-with-resources/src/main/java/br/com/mystudies/java/functional/ManagingLocks.java
+++ working-with-resources/src/main/java/br/com/mystudies/java/functional/ManagingLocks.java
@@ -1,20 +1,33 @@
 package br.com.mystudies.java.functional;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ManagingLocks {
 
 	
-	
-	
-	
 	public static class Locking{
+
+		private Lock lock = new ReentrantLock();
 		
+		public void doOp() {
+			Locker.runLocked(lock, () ->  {/*Critial code*/});
+		}			
 	}
 	
 	
 	
 	public static class Locker{
-		
+		public static void runLocked(Lock lock, Runnable block) {
+			lock.lock();			
+			try {
+				block.run();
+			} finally {
+				lock.unlock();
+			}
+		}
 	}
 	
+
 	
 }
