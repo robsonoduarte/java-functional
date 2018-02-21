2e7cc617f50ff92a67ddb72646386fd6dd27b954
Robson Duarte <robson.o.d@gmail.com>
2018-02-21 18:54:50 -0300

    starting new example.

---
+++ working-with-resources/src/main/java/br/com/mystudies/java/functional/ManagingLocks.java
@@ -0,0 +1,20 @@
+package br.com.mystudies.java.functional;
+
+public class ManagingLocks {
+
+	
+	
+	
+	
+	public static class Locking{
+		
+	}
+	
+	
+	
+	public static class Locker{
+		
+	}
+	
+	
+}
